dded43531ed9b7cd05b1bb93559ee002f49181e3
Pablito <skyyshoopro@gmail.com>
2026-01-07 18:18:12 -0500

    fini

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -11,6 +11,35 @@ import { bounce, shakeX, tada } from 'ng-animate';
 export class AppComponent {
   title = 'ngAnimations';
 
+
   constructor() {
+  }
+
+  rotate= false;
+  one = 0;
+  two = 0;
+  three = 0;
+
+  rotateCube() {
+    if(this.rotate == false){
+      this.rotate = true;
+      setTimeout(() => {this.rotate = false;},2000);
+    }
+  }
+
+  playNgAnimations(forever:boolean) {
+    this.one++;
+    setTimeout(() => {
+      this.two++;
+      setTimeout(() => {
+        this.three++;
+        if(forever)
+        {
+          setTimeout(() => {
+            this.playNgAnimations(true);
+          },3000);
+        }
+      },3000);
+    },2000);
   }
 }
